a1d1095496551b68b6861b30087f8f383c6c2856
phuocpt <phantienphuoc98@gmail.com>
2026-07-10 17:03:37 +0700

    build

---
+++ products/Invitation/Wedding/khach_thien-ngoe-my-dan/guests.js
@@ -0,0 +1,14 @@
+var GUEST_LIST = [
+    { id: 1, name: 'Vợ chồng chị Dung (HN)' },
+    { id: 2, name: 'Vợ chồng anh Định (HN)' },
+    { id: 3, name: 'Vợ chồng chị Hương (Tánh Linh)' },
+    { id: 4, name: 'Chị Hạnh + (Sài Gòn)' },
+    { id: 5, name: 'Chị Ngọc Nhung' },
+    { id: 6, name: 'Gia đình bạn Hiếu Lễ' },
+    { id: 7, name: 'MC Hy Văn' },
+    { id: 8, name: 'Ca sỹ Quỳnh Cầm' },
+    { id: 9, name: 'Bạn Thanh Vinh +' },
+    { id: 10, name: 'Vợ chồng bạn Duy Khoa' },
+    { id: 11, name: 'Chị My anh Thịnh' },
+    { id: 12, name: 'Gia đình Nội 5 USA' },
+];
